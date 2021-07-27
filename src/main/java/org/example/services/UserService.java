c2a39c1033bad1ce02925df9049b46b5c14449a8
Helen Silva <helen.silva.1406@gmail.com>
2021-07-27 15:52:34 -0300

    Exception handling - delete

--- src/main/java/org/example/services/UserService.java
+++ src/main/java/org/example/services/UserService.java
@@ -2,8 +2,11 @@ package org.example.services;
 
 import org.example.entities.User;
 import org.example.repositories.UserRepository;
+import org.example.services.expections.DatabaseException;
 import org.example.services.expections.ResourcesNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,15 @@ public class UserService {
     }
 
     public void delete(Long id){
-      repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourcesNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
